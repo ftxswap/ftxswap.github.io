30fee14a662ab92a625c3fd98fc33aed876246a1
ftxswap <baconBeacon@protonmail.com>
2020-06-26 20:27:14 -0700

    Updates

---
+++ precache-manifest.43daf0b2780f15dda67ff53aa53c9596.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b21a5891b84fd576a3ec893c8a63842",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f818672420b5c3acd481",
+    "url": "./static/css/3.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "bf60fb56e8f9334118e3",
+    "url": "./static/js/0.d8a52274.chunk.js"
+  },
+  {
+    "revision": "f818672420b5c3acd481",
+    "url": "./static/js/3.fdc398ea.chunk.js"
+  },
+  {
+    "revision": "42f336e17c9a028d732bfe9cb4667a6a",
+    "url": "./static/js/3.fdc398ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a975b0b1d664258852af",
+    "url": "./static/js/4.ee3549a3.chunk.js"
+  },
+  {
+    "revision": "300191e0e0c250a7b97e",
+    "url": "./static/js/5.d78cd86d.chunk.js"
+  },
+  {
+    "revision": "19a028d53302ea004e13",
+    "url": "./static/js/6.4ca9950b.chunk.js"
+  },
+  {
+    "revision": "d828d064fd611903189974745a0859f2",
+    "url": "./static/js/6.4ca9950b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af5c5d4e60d2b2e35e7",
+    "url": "./static/js/7.c0414267.chunk.js"
+  },
+  {
+    "revision": "091da06c11faa4786ff98801cf526ad7",
+    "url": "./static/js/7.c0414267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8468b7ecf93d60098c84",
+    "url": "./static/js/main.f1b85ccb.chunk.js"
+  },
+  {
+    "revision": "258a4bdee847850dcf60",
+    "url": "./static/js/runtime-main.316eb970.js"
+  },
+  {
+    "revision": "cd061363bbf9cd7a26cb09b642dcaf63",
+    "url": "./static/media/arrow-down-blue.cd061363.svg"
+  },
+  {
+    "revision": "c0dedd2f2ed0c4d07d7ca75af3f0a65f",
+    "url": "./static/media/arrow-down-grey.c0dedd2f.svg"
+  },
+  {
+    "revision": "337ad716bd89163e2a9c3495b7e0f029",
+    "url": "./static/media/arrow-right-white.337ad716.png"
+  },
+  {
+    "revision": "d285b6cf22b4f1552bb4009333462632",
+    "url": "./static/media/arrow-right.d285b6cf.svg"
+  },
+  {
+    "revision": "904b44c2b22eb2d49f618396e6f2db1a",
+    "url": "./static/media/blue-loader.904b44c2.svg"
+  },
+  {
+    "revision": "ed2a1dad16cb9a4b9afd788ddaae7290",
+    "url": "./static/media/circle-grey.ed2a1dad.svg"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "./static/media/circle.2d975615.svg"
+  },
+  {
+    "revision": "62578f5994645a1825d4829e2c4394b0",
+    "url": "./static/media/coinbaseWalletIcon.62578f59.svg"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "./static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "./static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b96d70e10dd30a64a0d122603577c8ae",
+    "url": "./static/media/dropup-blue.b96d70e1.svg"
+  },
+  {
+    "revision": "50c67f3cdd04281013ef95e92fc7244e",
+    "url": "./static/media/link.50c67f3c.svg"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "./static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "./static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "./static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "4f2c4440e19ed9428d0caae5d9840df6",
+    "url": "./static/media/menu.4f2c4440.svg"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "./static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "e8021e51723d455b2f9fa2446808d2a3",
+    "url": "./static/media/plus-blue.e8021e51.svg"
+  },
+  {
+    "revision": "d8e0be7d6efb0b53c37eb75e44b35bda",
+    "url": "./static/media/plus-grey.d8e0be7d.svg"
+  },
+  {
+    "revision": "b234b2bfa0417c7e8711c3a8d17afeec",
+    "url": "./static/media/portisIcon.b234b2bf.png"
+  },
+  {
+    "revision": "1ae4d9f4653371789d98b85139933d27",
+    "url": "./static/media/question-mark.1ae4d9f4.svg"
+  },
+  {
+    "revision": "a46e8bc1a36444be83a85007353d692f",
+    "url": "./static/media/question.a46e8bc1.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "./static/media/spinner.be00fc4a.svg"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "./static/media/trustWallet.edcc1ab5.png"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "./static/media/walletConnectIcon.8215855c.svg"
+  },
+  {
+    "revision": "b75565aead8e81b680926e35953d640e",
+    "url": "./static/media/wordmark.b75565ae.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "./static/media/wordmark_white.9914390f.svg"
+  },
+  {
+    "revision": "5b8e218668bfea1d44b887bd042f6a52",
+    "url": "./static/media/x.5b8e2186.svg"
+  }
+]);
